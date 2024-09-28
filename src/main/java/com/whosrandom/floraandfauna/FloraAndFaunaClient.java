9830d88f4fded28dad36b559fb1a9d9e8d92fa8f
RandomDev <mdmukunth@gmail.com>
2024-09-28 10:43:16 -0800

    Initial commit

---
+++ src/main/java/com/whosrandom/floraandfauna/FloraAndFaunaClient.java
@@ -0,0 +1,10 @@
+package com.whosrandom.floraandfauna;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FloraAndFaunaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/whosrandom/floraandfauna/FloraAndFaunaDataGenerator.java
@@ -0,0 +1,11 @@
+package com.whosrandom.floraandfauna;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FloraAndFaunaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
